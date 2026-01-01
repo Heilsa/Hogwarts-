e8363295942f7143557681f47f40090756cd0851
Heilsa <katushenko.a@mail.ru>
2026-01-01 22:05:46 +0700

    Revert "Hogwarts School"
    This reverts commit 9059c3259930ebd0671e658241c8b5e08fc774f2.

--- src/main/java/ru/hogwarts/school/repository/FacultyRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.hogwarts.school.repository;
-
-import ru.hogwarts.school.model.Faculty;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FacultyRepository extends JpaRepository<Faculty, Long> {
-
-}
--- src/main/java/ru/hogwarts/school/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.hogwarts.school.repository;
-
-import ru.hogwarts.school.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
