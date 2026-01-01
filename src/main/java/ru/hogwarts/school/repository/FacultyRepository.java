9d13383149d75a09f6c37b95d4f950387fcc1af1
Heilsa <katushenko.a@mail.ru>
2026-01-01 22:10:32 +0700

    Hogwarts School

---
+++ src/main/java/ru/hogwarts/school/repository/FacultyRepository.java
@@ -0,0 +1,8 @@
+package ru.hogwarts.school.repository;
+
+import ru.hogwarts.school.model.Faculty;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FacultyRepository extends JpaRepository<Faculty, Long> {
+
+}
---
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -0,0 +1,45 @@
+package ru.hogwarts.school.service;
+
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.repository.FacultyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
+public class FacultyService {
+
+    private final FacultyRepository facultyRepository;
+
+    @Autowired
+    public FacultyService(FacultyRepository facultyRepository) {
+        this.facultyRepository = facultyRepository;
+    }
+
+    public Faculty getFaculty(Long id) {
+        return facultyRepository.findById(id).orElse(null);
+    }
+
+    public Faculty updateFaculty(Faculty faculty) {
+        return facultyRepository.save(faculty);
+    }
+
+    public Faculty createFaculty(Faculty faculty) {
+        return facultyRepository.save(faculty);
+    }
+
+    public void deleteFaculty(Long id) {
+        facultyRepository.deleteById(id);
+    }
+
+    public Collection<Faculty> getAllFaculties() {
+        return facultyRepository.findAll();
+    }
+
+    public Collection<Faculty> getFacultiesByColor(String color) {  // было findByColor
+        return facultyRepository.findAll().stream()
+                .filter(faculty -> faculty.getColor().equalsIgnoreCase(color))
+                .toList();
+    }
+}
