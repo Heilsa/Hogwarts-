15f49e01efc7df907ffc531290ed61f5f9cca546
Heilsa <katushenko.a@mail.ru>
2026-03-18 23:49:50 +0700

    Hogwarts 1.3

--- src/main/java/ru/hogwarts/school/controller/StudentController.java
+++ src/main/java/ru/hogwarts/school/controller/StudentController.java
@@ -1,19 +1,21 @@
- package ru.hogwarts.school.controller;
+package ru.hogwarts.school.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
 import ru.hogwarts.school.service.StudentService;
 
+import javax.persistence.EntityNotFoundException;  // Добавь этот импорт
 import java.util.Collection;
 
 @RestController
 @RequestMapping("/student")
-@Tag(name = "Студенты", description = "API для управления студентами Хогвартса")  // Исправлено: Студенты
+@Tag(name = "Студенты", description = "API для управления студентами Хогвартса")
 public class StudentController {
     private final StudentService studentService;
 
@@ -73,6 +75,7 @@ public class StudentController {
             @RequestParam int maxAge) {
         return studentService.getStudentsByAgeBetween(minAge, maxAge);
     }
+
     @GetMapping("/{id}/faculty")
     public ResponseEntity<Faculty> getStudentFaculty(@PathVariable Long id) {
         Faculty faculty = studentService.getStudentFaculty(id);
@@ -81,6 +84,7 @@ public class StudentController {
         }
         return ResponseEntity.ok(faculty);
     }
+
     @PostMapping("/{studentId}/faculty/{facultyId}")
     @Operation(summary = "Назначить факультет студенту")
     public ResponseEntity<Student> assignStudentToFaculty(
@@ -93,5 +97,11 @@ public class StudentController {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(student);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<String> handleEntityNotFound(EntityNotFoundException e) {
+        return ResponseEntity.notFound().build();
     }
 }
